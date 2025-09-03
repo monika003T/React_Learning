f6e31f1d987373ce6ae0b24c7131096fe1ff18d2
monika003T <monikathakur2062@gmail.com>
2025-09-03 18:49:48 +0530

    day2

--- App.js
+++ App.js
@@ -1,7 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 //create element its a core react 
    
-    const heading= React.createElement("h1",{},"HELLO WORLD FROM REACT")
+    const heading= React.createElement("h1",{id:"heading"},"HELLO WORLD FROM REACT")
     //we need a root to render its, react dom is used 
     const root=ReactDOM.createRoot(document.getElementById('root'))
     //render heading
     root.render(heading);
+    console.log(heading)
+
+
+// how to create nested element
+/* 
+    <div id="parent">
+
+         <div id='child'>
+             <h1>"I'm nested element"</h1>
+         </div>
+
+    </div>
+ */
+
+    const parent=React.createElement('div',{id:"parent"},
+             React.createElement('div',{id:'child'},
+                React.createElement('h1',{id:"nested"},"I'm Nested Element")
+            ));
+     console.log(parent)
+    root.render(parent)
