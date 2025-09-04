ac144189b83097adb2e8c3b4c3782d4bf5c633a8
monika003T <monikathakur2062@gmail.com>
2025-09-04 15:52:46 +0530

    foundation

--- App.js
+++ App.js
@@ -1,31 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-
-//create element its a core react 
-   
-    const heading= React.createElement("h1",{id:"heading"},"HELLO WORLD FROM REACT")
-    //we need a root to render its, react dom is used 
-    const root=ReactDOM.createRoot(document.getElementById('root'))
-    //render heading
-    root.render(heading);
-    console.log(heading)
-
-
-// how to create nested element
-/* 
-    <div id="parent">
-
-         <div id='child'>
-             <h1>"I'm nested element"</h1>
-         </div>
-
-    </div>
- */
-
-    const parent=React.createElement('div',{id:"parent"},
-             React.createElement('div',{id:'child'},
-                React.createElement('h1',{id:"nested"},"I'm Nested Element")
-            ));
-     console.log(parent)
-    root.render(parent)
+// react element is an object => when render it is an html element 
+const heading=React.createElement('h1',{id:"heading"}, "namaste Monika")
+const root= ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading)
---
+++ practice/practice0.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+//create element its a core react 
+   
+    const heading= React.createElement("h1",{id:"heading"},"HELLO WORLD FROM REACT")
+    //we need a root to render its, react dom is used 
+    const root=ReactDOM.createRoot(document.getElementById('rr'))
+    //render heading
+    rr.render(heading);
+    console.log(heading)
+
+
+// how to create nested element
+/* 
+    <div id="parent">
+
+         <div id='child'>
+             <h1>"I'm nested element"</h1>
+         </div>
+
+    </div>
+ */
+
+    const parent=React.createElement('div',{id:"parent"},
+             React.createElement('div',{id:'child'},
+                React.createElement('h1',{id:"nested"},"I'm Nested Element")
+            ));
+     console.log(parent)
+    root.render(parent)
