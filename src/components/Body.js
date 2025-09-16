8d5207dcb7d268c8ec832c9722787d560866a047
monika003T <monikathakur2062@gmail.com>
2025-09-16 19:03:40 +0530

    hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,26 @@
 import Rescart from "./Rescart";
 import reslist from "../utils/mockdata";
+import { useState } from "react";
 
 const Body=()=>{
+    
+
+    // state variable
+    let [listofRestaurant, setListofRestaurant]=useState(reslist)
     return(
     <div className="body">
-        <div className="search">
-           search
+        <div className="filter">
+           <button className="filter-btn"
+            onClick={()=>{
+            const  filtered=listofRestaurant.filter((res)=>res.info.avgRating>4.5
+           );
+           setListofRestaurant(filtered)
+        }}>
+           Top Rated Restaurnt </button>
         </div>
         <div className="restuarant_container">
             {
-                reslist.map((restaurant)=>(
+                listofRestaurant.map((restaurant)=>(
                     <Rescart key={restaurant.info.id} resData={restaurant} />
                 ))
             }
